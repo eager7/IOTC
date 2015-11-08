5e5931cb04bf61c93767ef303067d7a03ac9efef
eager7 <panchangtao@gmail.com>
2015-11-08 22:43:51 +0800

    eager7:添加命令文档

--- iotc_master/include/iotc_command.h
+++ iotc_master/include/iotc_command.h
@@ -44,8 +44,8 @@ typedef enum
 {
     E_COMMAND_DEVICES_SEARCH                = 0x0001,
     E_COMMAND_DEVICES_REPORT                = 0x0002,
-    E_COMMAND_SET_ONOFF                     = 0x0003,
-    E_COMMAND_SOCKET_DISCONNECT             = 0x0004,
+    E_COMMAND_SOCKET_DISCONNECT             = 0x0003,
+    E_COMMAND_SET_ONOFF                     = 0x0004,
 }teIotcMessageType;
 
 typedef enum
